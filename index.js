60305c95725c07602496078453cdff04620f2f9b
Julien <julien3101@hotmail.com>
2025-05-24 21:35:09 +0200

    tout marche
    manque le rajout des projets, refonte du css

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const os = require("os");
 const multer = require("multer");
 const { PrismaClient } = require("@prisma/client");
 const bodyParser = require("body-parser");
@@ -33,38 +34,86 @@ hbs.registerPartials(path.join(__dirname, "views", "partials"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+function getPublicIp(){
+    const interfaces = os.networkInterfaces();
+    for (let interface in interfaces) {
+        for (let address of interfaces[interface]) {
+            // Vérifie si l'adresse est IPv4 et non une adresse de boucle
+            if (address.family === 'IPv4' && !address.internal) {
+                return address.address; // Retourne l'adresse IP publique
+            }
+        }
+    }
+    return '0.0.0.0';
+}
+
+//CHEMIN ROOT
 app.get("/", async (req,res)=>{
     const highlighted = await prisma.projects.findMany({
         where: {
             isHighlight : true,
         },
+        take: 5,
     });
     const projects = await prisma.projects.findMany();
     res.render("index",{highlighted, projects});
 })
 
+//CHEMIN A PROPOS
 app.get("/self",async (req,res)=>{
     res.render("self");
 })
 
+//CHEMIN CV
 app.get("/CV", async (req,res)=>{
     res.sendFile(path.join(__dirname, 'public/data/CV_Julien_Demaiziere.pdf'));
 })
 
+
+//CHEMIN PROJETS
 app.get("/projects", async (req,res)=>{
     const Projets = await prisma.projects.findMany();
     res.render("projects",{Projets});
 })
 
+
 app.get("/project/:nom",async (req,res)=>{
-    const Proj = await prisma.projects.findMany({
+    //récupere le projet
+    const Proj = await prisma.projects.findFirst({
         where: {
             projectName : req.params.nom,
         },
     })
-    res.render("projects/project", {Proj});
-})
+
+    //récupère les compétence associé
+    const Skills = await prisma.skills.findMany({
+    where: {
+        skillId: {
+            in: await prisma.appliedSkills.findMany({
+                where: {
+                    projectId: Proj.projectId,
+                },
+                select: {
+                    skillId: true, 
+                },
+            }).then(skills => skills.map(skill => skill.skillId)), 
+        },
+    },
+});
+    //récupère les images associé
+    const imgDir = path.join(__dirname,'public','image',Proj.projectName);
+
+    fs.readdir(imgDir, (err,files)=>{
+        const images = files.filter(file => /\.(jpg|jpeg|png)$/.test(file));
+
+        console.log()
+    res.render("projects/project", {Proj,Skills,images});
+    });
+});
+
+
 
 app.listen(PORT,()=>{
+    const publicIP = getPublicIp();
     console.log(`server running at http://localhost:${PORT}`);
 })
